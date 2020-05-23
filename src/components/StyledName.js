ec5b8cf96ae68eb5287c8ed85c0f3c3342a10372
Joshua Zahner <joshuazahner@gmail.com>
2020-05-23 13:39:48 -0500

    Extracted stylized name for reuse

---
+++ src/components/StyledName.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class StyledName extends React.Component {
+  render() {
+    return <>
+      <span className={"bold"}>Joshua</span> &nbsp;
+      <span className={"light"}>Zahner</span>
+    </>
+  }
+}
+
+export default StyledName
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 import Item from "../components/ItemBox"
+import StyledName from "../components/StyledName"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/index.css'
 
 const IndexPage = () => (
   <div id={"root"}>
-    <SEO title={"Home"} />
+    <SEO title={"Home"}/>
     <section className={"background header"}>
       <div className={"container-fluid container-hz"}>
         <div className={"image-container"}>
-          <img className={"round"} id={"profile"} src={""}/>
+          <img className={"round"} id={"profile"} src={""} alt={""}/>
         </div>
         <div className={"content-container"}>
           <h1 className={'display-4'}>
-            <span className={"bold"}>Joshua</span> &nbsp;
-            <span className={'light'}>Zahner</span>
+            <StyledName/>
           </h1>
           <h4>A software developer, <span className={"light"}>for scientists.</span></h4>
         </div>
@@ -33,27 +31,27 @@ const IndexPage = () => (
       <h2 className={"text-center big-line"}>for use in the biological, earth, and ocean sciences.</h2>
       <Link to={"#"} className={"btn btn-primary btn-lg"}>About Me</Link>
       <div className={"container-hz separate item-box"}>
-          <Item title={"Software"}
-                   short={
-                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
-                     "Nullam malesuada dignissim lorem in hendrerit."
-                    }
-                   link={"/software/"}
-          />
-          <Item title={"Research"}
-                   short={
-                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
-                     "Nullam malesuada dignissim lorem in hendrerit."
-                   }
-                   link={"/research/"}
-          />
-          <Item title={"Photography"}
-                   short={
-                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
-                     "Nullam malesuada dignissim lorem in hendrerit."
-                   }
-                   link={"/photos/"}
-          />
+        <Item title={"Software"}
+              short={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Nullam malesuada dignissim lorem in hendrerit."
+              }
+              link={"/software/"}
+        />
+        <Item title={"Research"}
+              short={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Nullam malesuada dignissim lorem in hendrerit."
+              }
+              link={"/research/"}
+        />
+        <Item title={"Photography"}
+              short={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Nullam malesuada dignissim lorem in hendrerit."
+              }
+              link={"/photos/"}
+        />
       </div>
     </section>
     <section className={"background header"}>
@@ -61,7 +59,8 @@ const IndexPage = () => (
         <h1 className={'display-4 bold text-center'}>
           From Docker to Dolphins
         </h1>
-        <h4 className={"separate text-center"}>The go to blog for a mix of programming, ocean science, and conservation topics.</h4>
+        <h4 className={"separate text-center"}>The go to blog for a mix of programming, ocean science, and conservation
+          topics.</h4>
         <Link to={"/blog"} className={"btn btn-primary"}>Read the blog</Link>
       </div>
     </section>

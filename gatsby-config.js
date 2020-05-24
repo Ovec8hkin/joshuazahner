63dfb31fcf901bdfe920d31ee07dc1ab53a2ac5b
Joshua Zahner <joshuazahner@gmail.com>
2020-05-24 00:20:50 -0500

    Update software_index.js to read markdown files from /src/content/software

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,20 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/content`
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/images`
+      }
+    },
+    `gatsby-transformer-remark`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/software_index.js
+++ src/pages/software_index.js
@@ -1,46 +1,57 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import {graphql} from "gatsby"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/index.css'
 import '../css/item.css'
-import Item from "../components/ItemBox"
-import { Link } from "gatsby"
-import mintpy from "../images/mintpy.png"
+
 import ImageItem from "../components/ImageItem"
 
 
 
-const SoftwareIndexPage = () => (
+const SoftwareIndexPage = ({data}) => (
   <Layout pageTitle={'Software'}>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu pharetra sem. Integer imperdiet nisi
       dapibus turpis volutpat sagittis. Nunc eleifend nisl nunc, quis vestibulum ante convallis eu. Sed in est in dolor
       luctus scelerisque. Phasellus lorem lorem, commodo in efficitur eu, vehicula non augue. Nam elementum nisi sed
       orci blandit, sed placerat est lacinia. Etiam fringilla euismod pellentesque.</p>
     <div className={"container-fluid container-hz separate item-box"}>
-      <ImageItem projectTitle={"MintPy"}
-                 projectBlurb={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu pharetra sem."}
-                 image={mintpy}
-                 tags={["Python", "parallel", "CLI Tool"]}
-      />
-      <ImageItem projectTitle={"joshuazahner.com"}
-                 projectBlurb={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu pharetra sem."}
-                 image={mintpy}
-                 tags={["Gatsby", "GraphQL", "Website"]}
-      />
-      <ImageItem projectTitle={"Miami GeoHazards"}
-                 projectBlurb={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu pharetra sem."}
-                 image={mintpy}
-                 tags={["React", "Flask API", "Website"]}
-      />
-      <ImageItem projectTitle={"Miami GeoHazards"}
-                 projectBlurb={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu pharetra sem."}
-                 image={mintpy}
-                 tags={["React", "Flask API", "Website"]}
-      />
+      {data.allMarkdownRemark.edges.map(software => {
+        data = software.node.frontmatter
+        return <ImageItem projectTitle={data.title}
+                          projectBlurb={data.blurb}
+                          image={data.image.childImageSharp.sizes.src}
+                          tags={data.tags}
+        />
+      })}
     </div>
   </Layout>
 )
 
 export default SoftwareIndexPage
+
+export const pageQuery = graphql`
+  query{
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter{
+            path
+            title
+            tags
+            blurb
+            image {
+              childImageSharp {
+                sizes(maxWidth: 1140) {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`

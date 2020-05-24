bfa1d27199e93ba2b55d3842c16f366daded8d7b
Joshua Zahner <joshuazahner@gmail.com>
2020-05-24 13:09:00 -0500

    Setup automatic page creation for all markdown files based on their frontmatter path

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,38 @@
  */
 
 // You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+  const softwareTemplate = require.resolve(`./src/templates/software.js`)
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___title] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: softwareTemplate,
+      context: {
+        // additional data can be passed via context
+        slug: node.frontmatter.path,
+      },
+    })
+  })
+}

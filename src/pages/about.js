72af522aac52f8b75eac09e1d8f571f08c83e1bd
Joshua Zahner <joshuazahner@gmail.com>
2020-06-13 13:35:43 -0500

    Added image to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,11 +11,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons"
 import {faTwitter, faGithub, faFacebook} from "@fortawesome/free-brands-svg-icons"
 
+import Img from 'gatsby-image'
+
+import about from '../images/about.jpg'
+
+console.log(about)
+
 const ResearchIndexPage = ({data}) => (
   <Layout pageTitle={'About Me'} active={"About"}>
-    <p>I picked up programming after joining my highschool FIRST robotics team in 2014, and quickly fell in love with software development. I progressed from writing simple autonomous control programs for my team's annual robot, to designing websites in raw HTML, CSS, and javascript (this sounds like a downgrade, but I can assure you it's not), in the good ol' days before React, Vue, Node, and the rest of the modern javascript ecosystem emerged. I continued to develop websites and even a few mobile apps for the next four years of high school.</p>
-    <p>In August 2017, I started at the University of Miami as a marine science and computer science double major. I have loved the ocean and all of the wildlife in it for as long as I can remember, and UM provided one of the very few programs where I could follow that passion in addition to the more stable field of computer science. I quickly found that my computational background and development skills were highly sought after in the ocean sciences domain, and so I began my journey into developing software for applications in the sciences.</p>
-    <p>Outside of programming and science, I greatly enjoy traveling to and experiencing each of America's National Parks, which make up a majority of my body of photographic work. I also spend extensive time reading (mostly science fiction and fantasy, but I throw some environmental non-fiction in there to mix things up every once in a while) and watching NHL hockey (Let's Go Blues!).</p>
+    <div className={"container-hz content-container text-left"}>
+      <div className={"content"}>
+        <p>I picked up programming after joining my highschool FIRST robotics team in 2014, and quickly fell in love with software development. I progressed from writing simple autonomous control programs for my team's annual robot, to designing websites in raw HTML, CSS, and javascript (this sounds like a downgrade, but I can assure you it's not), in the good ol' days before React, Vue, Node, and the rest of the modern javascript ecosystem emerged. I continued to develop websites and even a few mobile apps for the next four years of high school.</p>
+        <p>In August 2017, I started at the University of Miami as a marine science and computer science double major. I have loved the ocean and all of the wildlife in it for as long as I can remember, and UM provided one of the very few programs where I could follow that passion in addition to the more stable field of computer science. I quickly found that my computational background and development skills were highly sought after in the ocean sciences domain, and so I began my journey into developing software for applications in the sciences.</p>
+        <p>Outside of programming and science, I greatly enjoy traveling to and experiencing each of America's National Parks, which make up a majority of my body of photographic work. I also spend extensive time reading (mostly science fiction and fantasy, but I throw some environmental non-fiction in there to mix things up every once in a while) and watching NHL hockey (Let's Go Blues!).</p>
+      </div>
+      <div className={'sidebar'}>
+        <img src={about} />
+      </div>
+    </div>
+
     <div className={"separate"}>
       <h3>Contact me</h3>
       <div className={"inline"}>
